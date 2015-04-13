795a04da3590b90f2e31ac1fff00e63421cd3df2
Typ vonner Gosse <f-fechner@web.de>
2015-04-13 13:17:10 +0200

    Unity Projekt
    Das Unity Projekt vom aktuellem Stand

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour 
+{
+
+	private GameObject selectedUnit = null;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown (0)) 
+		{
+			select();
+		}
+
+		if(Input.GetMouseButtonDown (1)) 
+		{
+			move();
+		}
+	}
+
+	private void select()
+	{
+		Ray ray = GetComponent<UnityEngine.Camera>().ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		selectedUnit = null;
+
+		if(Physics.Raycast(ray, out hit))
+		{
+			GameObject obj = hit.collider.gameObject;
+			
+			if(obj != null)
+			{
+				if(obj.tag == "Unit")
+				{
+					//obj.GetComponent<Unit>().destination = new Vector2(0,0); <- Test
+					// Schreibe Einheit in Liste selektierter Einheiten
+					selectedUnit = obj;
+				}
+			}
+		}
+	}
+
+	private void move()
+	{
+		Ray ray = GetComponent<UnityEngine.Camera>().ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if(Physics.Raycast (ray, out hit))
+		{
+			if(selectedUnit != null)
+			{
+				selectedUnit.GetComponent<Unit>().destination = hit.point;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour 
+{
+	private int speed = 5;
+	public int life = 20;
+
+	public Vector3 destination;
+	
+	void Start()
+	{
+		destination = new Vector3(transform.position.x, 0, transform.position.z);
+		Debug.Log (destination);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float diffX = destination.x - transform.position.x;
+		float diffZ = destination.z - transform.position.z;
+		float diffTotal = Mathf.Sqrt(diffX * diffX + diffZ * diffZ);
+		
+		if(diffTotal > 1)
+		{
+			// Bewege dich zum Ziel
+			
+			Vector3 temp = new Vector3(diffX/diffTotal, 0, diffZ/diffTotal);
+			temp.Normalize();
+			temp = temp * Time.deltaTime * speed;
+			
+			transform.Translate(temp, Space.World);
+		}
+	}
+}
