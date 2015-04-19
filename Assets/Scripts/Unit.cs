1634174038bc630b2060269ecd81542302658462
Typ vonner Gosse <f-fechner@web.de>
2015-04-20 00:33:43 +0200

    NavMesh Basis

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,11 +5,14 @@ public class Unit : MonoBehaviour
 {
 	private int speed = 5;
 	public int life = 20;
+	NavMeshAgent agent;
+	public Transform target;
 
 	public Vector3 destination;
 	
 	void Start()
 	{
+		agent = GetComponent<NavMeshAgent>();
 		destination = new Vector3(transform.position.x, 0, transform.position.z);
 		Debug.Log (destination);
 	}
@@ -17,19 +20,7 @@ public class Unit : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		float diffX = destination.x - transform.position.x;
-		float diffZ = destination.z - transform.position.z;
-		float diffTotal = Mathf.Sqrt(diffX * diffX + diffZ * diffZ);
-		
-		if(diffTotal > 1)
-		{
-			// Bewege dich zum Ziel
-			
-			Vector3 temp = new Vector3(diffX/diffTotal, 0, diffZ/diffTotal);
-			temp.Normalize();
-			temp = temp * Time.deltaTime * speed;
-			
-			transform.Translate(temp, Space.World);
-		}
+		agent.SetDestination (destination);
 	}
+	
 }
